50ded4de30139144f2afafe5cc28302cef795b8c
Maxine Chui <maxine.chui@gmail.com>
2017-10-27 10:07:49 -0700

    Auth hw

--- W4D4/app/models/user.rb
+++ W4D4/app/models/user.rb
@@ -4,5 +4,38 @@ class User < ApplicationRecord
   validates :password, length: { minimum: 6, allow_nill: true }
   before_validation :ensure_session_token
 
-  
+  attr_reader :password
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    if user && BCrypt::Password.new(user.password_digest).is_password?(password)
+      user
+    else
+      nil
+    end
+  end
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64
+  end
+
+  def reset_session_token
+    self.session_token = User.generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
 end
